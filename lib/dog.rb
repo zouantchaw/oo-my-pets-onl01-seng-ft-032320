0435a588fe5d77d6808434130d17a8f65dbb5dfe
Wielfried Zouantcha <zouantchaw74@gmail.com>
2020-04-08 12:50:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+  attr_accessor :owner, :mood 
+  attr_reader :name 
+  
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner
+    @mood = "nervous"
+    save 
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @species = "human"
+    save 
+  end 
+  
+  def say_species
+    return "I am a #{species}."
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+    all.count
+  end 
+  
+  def self.reset_all 
+    all.clear 
+  end 
+  
+  #Associations 
+  def cats 
+     Cat.all.select{|cats|
+      cats.owner == self 
+     }
+  end 
+  
+  def dogs 
+     Dog.all.select{|dogs|
+      dogs.owner == self 
+     }
+  end 
+  
+  def buy_cat(name)
+    new_cat = Cat.new(name, self)
+    cats << new_cat
+  end
+  
+  def buy_dog(name)
+    new_dog = Dog.new(name, self)
+    dogs << new_dog
+  end
+  
+  def walk_dogs
+    Dog.all.each{|dog|
+      dog.mood = "happy"
+    }
+  end 
+  
+  def feed_cats 
+    Cat.all.each{|cat|
+      cat.mood = "happy"
+    }
+  end 
+  
+  def sell_pets 
+    
+  end 
+  
 end
